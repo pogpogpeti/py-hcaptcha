beb5d6864cc1d9b2fe186cb1c39798d3ddf5eb16
no u <57400769+pogpogpeti@users.noreply.github.com>
2022-01-16 12:48:57 +0200

    Update __init__.py

--- hcaptcha/proofs/__init__.py
+++ hcaptcha/proofs/__init__.py
@@ -1,9 +1,9 @@
 from . import hsl
 from . import hsw
 
-def get_proof(type, data, hsw: str):
+def get_proof(type, data, hsw):
     if type == "hsl":
         return hsl.get_proof(data)
     elif type == "hsw":
-        return hsw.get_proof(data, hsw)
+        return hsw.get_proof(data, str(hsw))
     raise Exception(f"Unrecognized proof type '{type}'")
