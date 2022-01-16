d902d10888a64cc6d8f8b41704b26df4d1585c31
no u <57400769+pogpogpeti@users.noreply.github.com>
2022-01-16 12:29:40 +0200

    Update challenges.py

--- hcaptcha/challenges.py
+++ hcaptcha/challenges.py
@@ -32,7 +32,6 @@ class Challenge:
         self,
         site_key: str,
         site_url: str,
-        hsw_loc: str,
         data: Union[dict, None] = None,
         agent: Agent = None,
         http_client: HTTPClient = None,
@@ -59,7 +58,7 @@ class Challenge:
         self._agent = agent or random_agent()
         self._http_client = http_client or HTTPClient(**http_kwargs)
         self.model = model
-        with open(self._hsw_loc, "r") as f:
+        with open("hcaptcha-js/hsw.js", "r") as f:
             self.hsw = f.read()
 
         self.id = None
